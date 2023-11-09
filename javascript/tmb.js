27e1cbc6a362dbbc7297e7bf1607c9b1dab0f772
Alexandre Oliveira <107891784+estudoalexandre@users.noreply.github.com>
2023-11-09 15:49:56 -0300

    Correcao bug html e js para calculo

--- javascript/tmb.js
+++ javascript/tmb.js
@@ -21,22 +21,24 @@ const CalculadoraTMB = {
         if(fatorNivelAtividade){
             return total * fatorNivelAtividade;
         }
+
+        return total;
     }
 };
 
 const nome = document.querySelector('.form-control[name="nome"]');
-const nivelAtividade = document.querySelector('.form-select[name="nivelAtividade"]');
 const botaoCalcular = document.querySelector('.btn.btn-primary');
 const sexo = document.querySelector('.form-select[name="sexo"]');
 
 botaoCalcular.addEventListener('click',function() {
+    const nivelAtividade = document.querySelector('.form-select[name="nivelAtividade"]');
     const altura = parseFloat(document.querySelector('.form-control[name="altura"]').value);
     const peso = parseFloat(document.querySelector('.form-control[name="peso"]').value);
     const idade = parseFloat(document.querySelector('.form-control[name="idade"]').value);
     const displayResultado = document.querySelector('.resultado');
 
 
-    const resultado = CalculadoraTMB.calcularTMB(altura, peso, idade);
+    const resultado = CalculadoraTMB.calcularTMB(altura, peso, idade, nivelAtividade);
 
     return displayResultado.innerHTML = resultado;
 });
