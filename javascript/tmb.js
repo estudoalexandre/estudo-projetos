92bd7f516806d34281d587035d2411615d2bc72d
Alexandre Oliveira <107891784+estudoalexandre@users.noreply.github.com>
2023-11-10 17:59:33 -0300

    Diminuindo código e atribuindo funcao data-fator no HTML para pegar os dados padrão de Atividade

--- javascript/tmb.js
+++ javascript/tmb.js
@@ -1,74 +1,52 @@
-
 const CalculadoraTMB = {
-    nivelAtividadeFatores: {
-        sedentarismo: 1.2,
-        leve: 1.375,
-        moderado: 1.55,
-        ativo: 1.725,
-        extremamente: 1.9
-    },
-    calcularTMBMan: function (altura, peso, idade, nivelAtividade) {
-        let total;
-
+    calcularTMBMan: function (altura, peso, idade) {
         peso = 66 + (13.7 * peso);
         altura = 5 * altura;
         idade = 6.8 * idade;
 
-        total = peso + altura - idade;
-
-        const fatorNivelAtividade = this.fatorNivelAtividade(nivelAtividade);
-
-        if (fatorNivelAtividade) {
-            return total * fatorNivelAtividade;
-        }
-
-        return total;
+        return peso + altura - idade;
     },
 
-    calcularTMBWoman: function (altura, peso, idade, nivelAtividade) {
-        let total;
+    calcularTMBWoman: function (altura, peso, idade) {
+        altura = 3.098 * altura;
+        peso = 447.593 + (9.247 * peso);
+        idade = 4.330 * idade;
 
-        altura = 3.098 * altura
-        peso = 447.593 + (9.247 * peso)
-        idade = 4.330 * idade
-
-        total = peso + altura - idade;
-
-        const fatorNivelAtividade = this.fatorNivelAtividade(nivelAtividade);
-
-        if(fatorNivelAtividade){
-            return total * fatorNivelAtividade
-        }
-
-        return total;
-        
-    },
-    fatorNivelAtividade: function (nivelAtividade){
-        return this.nivelAtividadeFatores[nivelAtividade];
+        return peso + altura - idade;
     }
 };
 
-const nome = document.querySelector('.form-control[name="nome"]');
 const botaoCalcular = document.querySelector('.btn.btn-primary');
 
 botaoCalcular.addEventListener('click', function () {
-    const sexoSelecionado = document.querySelector('.form-select[name="sexo"]').value;
-    const nivelAtividade = document.querySelector('.form-select[name="nivelAtividade"]').value;
     const altura = parseFloat(document.querySelector('.form-control[name="altura"]').value);
     const peso = parseFloat(document.querySelector('.form-control[name="peso"]').value);
     const idade = parseFloat(document.querySelector('.form-control[name="idade"]').value);
+    const nivelAtividadeSelect = document.querySelector('.form-select[name="nivelAtividade"]');
+    const fatorNivelAtividade = parseFloat(nivelAtividadeSelect.options[nivelAtividadeSelect.selectedIndex].getAttribute('data-fator'));
+
+    // const fatorNivelAtividade = parseFloat(nivelAtividadeSelect.value);
+
+    const sexoSelecionado = document.querySelector('.form-select[name="sexo"]').value;
+    const objetivoSelecionado = document.querySelector('.form-select[name="objetivo"]').value;
     const displayResultado = document.querySelector('.resultado');
 
     let resultado;
 
-    if(sexoSelecionado === "homem"){
-        resultado = CalculadoraTMB.calcularTMBMan(altura, peso, idade, nivelAtividade);
-    } else {
-        resultado = CalculadoraTMB.calcularTMBWoman(altura, peso, idade, nivelAtividade);
+    if (sexoSelecionado === "homem") {
+        resultado = CalculadoraTMB.calcularTMBMan(altura, peso, idade);
+    } else if (sexoSelecionado === "mulher") {
+        resultado = CalculadoraTMB.calcularTMBWoman(altura, peso, idade);
     }
 
-    displayResultado.innerHTML = resultado;
+    let TMBComNivelAtividade = resultado * fatorNivelAtividade;
+    
+    if (objetivoSelecionado === "perder") {
+        TMBComNivelAtividade -= 500;
+    } else if (objetivoSelecionado === "ganhar") {
+        TMBComNivelAtividade += 500;
+    }
+    
+    displayResultado.innerHTML = `Resultado: ${TMBComNivelAtividade.toFixed(2)} calorias por dia`;
 
 });
-
-
